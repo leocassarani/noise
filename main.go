0945c52682e0db3737716d5a8ee35a04f24d1fd0
Leo Cassarani <me@leocassarani.net>
2016-05-02 16:41:27 +0000

    The error rate may be specified using the "-rate" command-line flag
    The flag is optional, and the default value remains 0.25.

--- main.go
+++ main.go
@@ -2,14 +2,18 @@ package main
 
 import (
 	"bufio"
+	"flag"
 	"fmt"
 	"io"
 	"os"
 )
 
 func main() {
+	errorRate := flag.Float64("rate", 0.25, "error rate, must be in the [0,1) range")
+	flag.Parse()
+
 	r := bufio.NewReader(os.Stdin)
-	w := NewWriter(os.Stdout, 0.25)
+	w := NewWriter(os.Stdout, *errorRate)
 
 	for {
 		b, err := r.ReadByte()
